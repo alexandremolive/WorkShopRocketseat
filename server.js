9642043b5c0f3c87c0fa0955aba70772541a6fa4
Alexandre Oliveira <alexandremolive@gmail.com>
2020-06-14 17:29:07 -0300

    atualizaçao server

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-console.log("Olá")
+// usei o express para criar e configurar meu servidor
+const express = require("express")
+const server = express()
+
+
+// configurar arquivos estáticos (css, cripts, imagens)
+server.use(express.static("public"))
+
+
+//  configuração Nunjucjs
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("views", {
+express: server,
+noCache: true,
+})
+
+
+// criei uma rota 
+// e capturo o pedido do cliente para responder
+server.get("/", function(req,res){
+    return res.render("index.html")
+})
+
+server.get("/ideias", function(req,res){
+    return res.render("ideias.html")
+
+})
+// Liguei meu servidor na porta 3000
+// npm i nodemon monitoramento do node
+server.listen(3000)
+
+
